81817740b92e39dc5d64d0e44b1e757ba5eec2f5
Gentry Riggen <gentryriggen@outlook.com>
2015-05-03 22:04:07 -0600

    Initial Code Commit

---
+++ PartyUp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PartyUp.Startup))]
+namespace PartyUp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
