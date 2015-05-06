56043069e2e08e7874179f16a39b9a3bcf54a84a
Gentry Riggen <gentryriggen@outlook.com>
2015-05-05 21:08:39 -0600

    Some models and data updates

---
+++ PartyUp.data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using PartyUp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartyUp.data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+
+    }
+}
