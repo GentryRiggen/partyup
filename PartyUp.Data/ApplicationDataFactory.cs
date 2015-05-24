30c4c26473dfbab01c42636a473b98a9fb98ca07
Gentry Riggen <GentryRiggen@outlook.com>
2015-05-24 09:51:17 -0600

    Got data repos and factories. JWTs. App Settings

---
+++ PartyUp.Data/ApplicationDataFactory.cs
@@ -0,0 +1,48 @@
+﻿using PartyUp.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartyUp.Data
+{
+    public class ApplicationDataFactory : IDisposable
+    {
+        private ApplicationDbContext _context = new ApplicationDbContext();
+        private UserRepository _users = null;
+
+        public UserRepository Users
+        {
+            get
+            {
+                if (this._users == null)
+                {
+                    this._users = new UserRepository(this._context);
+                }
+                return this._users;
+            }
+        }
+
+        public ApplicationDbContext Context
+        {
+            get
+            {
+                return _context;
+            }
+        }
+
+        public void SaveChanges()
+        {
+            this._context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            if (this._context != null)
+            {
+                this._context.Dispose();
+            }
+        }
+    }
+}
---
+++ PartyUp.Data/Repositories/UserRepository.cs
@@ -0,0 +1,25 @@
+﻿using PartyUp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartyUp.Data.Repositories
+{
+    public class UserRepository : GenericRepository<User>
+    {
+        public UserRepository(ApplicationDbContext context) : base(context) { }
+
+        public User GetById(string id)
+        {
+            return base.DbSet.Where(u => u.Id.Equals(id)).FirstOrDefault();
+        }
+
+        public User FindByToken(string token)
+        {
+            return null;
+        }
+
+    }
+}
